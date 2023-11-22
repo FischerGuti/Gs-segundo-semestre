30a75450e9f72970964aa098a6dc338a3a9dbd54
FischerGuti <vfischer634@gmail.com>
2023-11-22 19:52:30 -0300

    primeiro

---
+++ terraform/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.23"
+    }
+  }
+  
+  backend "s3" {
+    bucket         = "gsquevaidarcertodofishinho"
+    key            = "terraform.tfstate"
+    dynamodb_table = "tabeladodofishinho"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
